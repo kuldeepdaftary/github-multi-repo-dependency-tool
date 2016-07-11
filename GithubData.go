46324e6730d2977a9b1bacdc9aaad06dd22f9db7
tcrands <tcrands@gmail.com>
2016-07-11 14:09:47 +0100

    retrieved statues url

---
+++ GithubData.go
@@ -0,0 +1,20 @@
+package main
+
+type GithubDataPr struct {
+	Action string `json:"action"`
+	Pull_Request struct {
+		Url string `json:"url"`
+        Title string `json:"title"`
+        User struct {
+            Login string `json:"login"`
+        }
+        Body string `json:"body"`
+        Statuses_Url string `json:"statuses_url"`
+	}
+}
+type GithubDataPrSet []GithubDataPr
+
+type GithubDataDependency struct {
+	State string `json:"state"`
+}
+type GithubDataDependencySet []GithubDataDependency
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "time"
+    "fmt"
+)
+
+func main() {
+    fmt.Println("Aloha")
+
+    router := NewRouter()
+
+    server := &http.Server{
+    	Addr: ":8080",
+    	Handler: router,
+    	ReadTimeout: 10 * time.Second,
+    	WriteTimeout: 10 * time.Second,
+    	MaxHeaderBytes: 1 << 20,
+    }
+    log.Fatal(server.ListenAndServe())
+}
