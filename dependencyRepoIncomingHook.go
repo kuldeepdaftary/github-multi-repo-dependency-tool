283b0d45690d5c34e73f77f53c3f643a47f87962
tcrands <tcrands@gmail.com>
2016-09-27 11:50:22 +0100

    Update dependencyRepoIncomingHook.go

--- dependencyRepoIncomingHook.go
+++ dependencyRepoIncomingHook.go
@@ -21,6 +21,7 @@ func dependencyRepoIncomingHook(w http.ResponseWriter, r *http.Request) {
     if githubDataPr.Pull_Request.Merged == true {
 	fmt.Println("MERGED IS TRUE")
 	depUrl := strings.Replace(githubDataPr.Pull_Request.Url, "api.github.com/repos", "github.com", 1)
+	fmt.Println(depUrl) 
         exists, url := checkDatabase(depUrl)
         if exists == true {
 	    fmt.Println("EXISTS IS TRUE")
