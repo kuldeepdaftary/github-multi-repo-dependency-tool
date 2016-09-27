dd8f9cdbf281ac25d712eb87ff91341f11d53794
tcrands <tcrands@gmail.com>
2016-09-27 11:27:18 +0100

    Update dependencyRepoIncomingHook.go

--- dependencyRepoIncomingHook.go
+++ dependencyRepoIncomingHook.go
@@ -17,7 +17,7 @@ func dependencyRepoIncomingHook(w http.ResponseWriter, r *http.Request) {
 	}
 
     if githubDataPr.Pull_Request.Merged == true {
-	depUrl := strings.Replace(githubDataPr.Pull_Request.Url, "github.com", "api.github.com/repos", 1)
+	depUrl := strings.Replace(githubDataPr.Pull_Request.Url, "api.github.com/repos", "github.com", 1)
         exists, url := checkDatabase(depUrl)
         if exists == true {
             go changePrStatus(url, "success", "atlas-roku")
